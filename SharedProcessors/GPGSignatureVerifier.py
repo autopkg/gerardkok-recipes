autopkg/gerardkok-recipes

commit 1d4a6cc25e16d299e4ac927907cb1b9df9a3ecb3

Ensure regex has r prefix

SharedProcessors/GPGSignatureVerifier.py
@@ -28,7 +28,7 @@ __all__ = ["GPGSignatureVerifier"]
 
 
 def check_for_goodsig(string):
-    return re.search("^\\[GNUPG:\\] GOODSIG ([0-9A-F]{8,})", string, re.M)
+    return re.search(r"^\\[GNUPG:\\] GOODSIG ([0-9A-F]{8,})", string, re.M)
 
 
 class GPGSignatureVerifier(Processor):

commit cde7c79d1cefd65ea2bddad8bc68ae4c241aead9

Fix regression in re.search
I think #1d4a6cc25e16d299e4ac927907cb1b9df9a3ecb3 broke the regex

Example:
```
>>> s = """
... [GNUPG:] NEWSIG\ngpg: Signature made Sat Aug 24 23:52:23 2019 CEST\ngpg:                using RSA key 8C31E5A17DD5D932B448FE1DE8A664480D9E43F5\n[GNUPG:] KEY_CONSIDERED 85E38F69046B44C1EC9FB07B76D78F0500D026C4 0\n[GNUPG:] SIG_ID TmBpEFp3cLGQ6E6jgjCZhMy0m0U 2019-08-24 1566683543\n[GNUPG:] KEY_CONSIDERED 85E38F69046B44C1EC9FB07B76D78F0500D026C4 0\ngpg: checking the trustdb\ngpg: no ultimately trusted keys found\n[GNUPG:] GOODSIG E8A664480D9E43F5 GPGTools Team <team@gpgtools.org>\ngpg: Good signature from "GPGTools Team <team@gpgtools.org>" [unknown]\ngpg:                 aka "[jpeg image of size 5871]" [unknown]\n[GNUPG:] VALIDSIG 8C31E5A17DD5D932B448FE1DE8A664480D9E43F5 2019-08-24 1566683543 0 4 0 1 8 00 85E38F69046B44C1EC9FB07B76D78F0500D026C4\n[GNUPG:] KEY_CONSIDERED 85E38F69046B44C1EC9FB07B76D78F0500D026C4 0\n[GNUPG:] KEY_CONSIDERED 85E38F69046B44C1EC9FB07B76D78F0500D026C4 0\n[GNUPG:] TRUST_UNDEFINED
... """
>>> print s

[GNUPG:] NEWSIG
gpg: Signature made Sat Aug 24 23:52:23 2019 CEST
gpg:                using RSA key 8C31E5A17DD5D932B448FE1DE8A664480D9E43F5
[GNUPG:] KEY_CONSIDERED 85E38F69046B44C1EC9FB07B76D78F0500D026C4 0
[GNUPG:] SIG_ID TmBpEFp3cLGQ6E6jgjCZhMy0m0U 2019-08-24 1566683543
[GNUPG:] KEY_CONSIDERED 85E38F69046B44C1EC9FB07B76D78F0500D026C4 0
gpg: checking the trustdb
gpg: no ultimately trusted keys found
[GNUPG:] GOODSIG E8A664480D9E43F5 GPGTools Team <team@gpgtools.org>
gpg: Good signature from "GPGTools Team <team@gpgtools.org>" [unknown]
gpg:                 aka "[jpeg image of size 5871]" [unknown]
[GNUPG:] VALIDSIG 8C31E5A17DD5D932B448FE1DE8A664480D9E43F5 2019-08-24 1566683543 0 4 0 1 8 00 85E38F69046B44C1EC9FB07B76D78F0500D026C4
[GNUPG:] KEY_CONSIDERED 85E38F69046B44C1EC9FB07B76D78F0500D026C4 0
[GNUPG:] KEY_CONSIDERED 85E38F69046B44C1EC9FB07B76D78F0500D026C4 0
[GNUPG:] TRUST_UNDEFINED

SharedProcessors/GPGSignatureVerifier.py
@@ -28,7 +28,7 @@ __all__ = ["GPGSignatureVerifier"]
 
 
 def check_for_goodsig(string):
-    return re.search(r"^\\[GNUPG:\\] GOODSIG ([0-9A-F]{8,})", string, re.M)
+    return re.search(r"^\[GNUPG:\] GOODSIG ([0-9A-F]{8,})", string, re.M)
 
 
 class GPGSignatureVerifier(Processor):